91061dc2d18ec942eabe78401dc87973cf5dc9b1
JiEung2 <181757@jnu.ac.kr>
2024-10-31 00:42:57 +0900

    feat(userService): user service 추가 및 회원가입 기능 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/user/service/UserService.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/user/service/UserService.java
@@ -1,4 +1,21 @@
 package com.yeojiphap.choki.domain.user.service;
 
+import static com.yeojiphap.choki.domain.user.message.UserSuccessMessage.*;
+
+import com.yeojiphap.choki.domain.user.domain.User;
+import com.yeojiphap.choki.domain.user.dto.signUpRequest;
+import com.yeojiphap.choki.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public String signUp(signUpRequest signUpRequest) {
+        User user = signUpRequest.toEntity();
+        userRepository.save(user);
+        return SIGN_UP_SUCCESS.getMessage();
+    }
 }
