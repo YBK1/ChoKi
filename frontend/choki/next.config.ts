eb5723602630922ecf1b5798614596832f9203b5
인호현 <inhohyun36@gmail.com>
2024-10-30 11:41:29 +0900

    fix: 서비스 워커 등록 코드 수정

--- frontend/choki/next.config.ts
+++ frontend/choki/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const withPWA = require('next-pwa')({
-	dest: 'public',
+	dest: 'public', // PWA 파일이 저장될 위치 설정
+	register: true, // 서비스 워커를 등록
+	skipWaiting: true, // 새로운 서비스 워커가 대기하지 않고 즉시 활성화
 });
 const nextConfig: NextConfig = {
 	/* config options here */
--- frontend/choki/src/pages/_app.tsx
+++ frontend/choki/src/pages/_app.tsx
@@ -1,25 +1,6 @@
-import { useEffect } from 'react';
-import { AppProps } from 'next/app';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	useEffect(() => {
-		// 초기에 서비스 워커를 등록합니다.
-		if ('serviceWorker' in navigator) {
-			navigator.serviceWorker
-				.register('/sw.js')
-				.then(registration => {
-					console.log(
-						'Service Worker registered with scope:',
-						registration.scope,
-					);
-				})
-				.catch(error => {
-					console.error('Service Worker registration failed:', error);
-				});
-		}
-	}, []);
-
+export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
 }
-
-export default MyApp;
