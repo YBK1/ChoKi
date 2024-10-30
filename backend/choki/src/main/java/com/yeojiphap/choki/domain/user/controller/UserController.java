6d2cfaaeacf22de774d33a5ff9d62bf3de45ef95
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-10-30 18:01:50 +0900

    feat: JWTutil 삭제

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/user/controller/UserController.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/user/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.yeojiphap.choki.domain.user.controller;
 
-import com.yeojiphap.choki.domain.user.dto.response.ChildProfileDto;
 import com.yeojiphap.choki.domain.user.dto.response.OtherChildResponseDto;
-import com.yeojiphap.choki.domain.user.service.JWTService;
 import com.yeojiphap.choki.domain.user.service.UserService;
 import com.yeojiphap.choki.global.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +12,6 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
-    private final JWTService jwtService;
-
-    @GetMapping("/user/myPage")
-    public ApiResponse<ChildProfileDto> getMyInfo(@RequestHeader("Authorization") String token) {
-        return ApiResponse.success(jwtService.getUserByToken(token));
-    }
 
     @GetMapping(value = "/user/{userId:[0-9]+}")
     public ApiResponse<OtherChildResponseDto> getUser(@PathVariable Long userId) {
