124e7460f67adf8b5f8f000c98631004c8d7f4e7
인호현 <inhohyun36@gmail.com>
2024-10-30 10:30:44 +0900

    feat: pwa 초기 설정

--- frontend/choki/src/pages/_app.tsx
+++ frontend/choki/src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		// 초기에 서비스 워커를 등록합니다.
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker
+				.register('/sw.js')
+				.then(registration => {
+					console.log(
+						'Service Worker registered with scope:',
+						registration.scope,
+					);
+				})
+				.catch(error => {
+					console.error('Service Worker registration failed:', error);
+				});
+		}
+	}, []);
+
 	return <Component {...pageProps} />;
 }
+
+export default MyApp;
