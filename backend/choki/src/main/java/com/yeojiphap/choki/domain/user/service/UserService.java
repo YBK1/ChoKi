e44fe1c39f92bc34802e26c5cdeb270db51849fe
JiEung2 <181757@jnu.ac.kr>
2024-10-31 01:04:52 +0900

    fix(userService): 비밀번호 암호화 코드 추가
    - 비밀번호를 암호화하여 인자로 전달

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/user/service/UserService.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/user/service/UserService.java
@@ -6,15 +6,18 @@ import com.yeojiphap.choki.domain.user.domain.User;
 import com.yeojiphap.choki.domain.user.dto.signUpRequest;
 import com.yeojiphap.choki.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public String signUp(signUpRequest signUpRequest) {
-        User user = signUpRequest.toEntity();
+        String encodedPassword = bCryptPasswordEncoder.encode(signUpRequest.userPassword());
+        User user = signUpRequest.toEntity(encodedPassword);
         userRepository.save(user);
         return SIGN_UP_SUCCESS.getMessage();
     }
