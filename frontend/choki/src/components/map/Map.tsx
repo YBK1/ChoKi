c716d6c552e2dae82f1e060d2b8961141605e801
piilll <spchoi1998@naver.com>
2024-10-31 22:59:38 +0900

    fix: 현재위치 기준 마커 찍히도록 설정

--- frontend/choki/src/components/map/Map.tsx
+++ frontend/choki/src/components/map/Map.tsx
@@ -1,31 +1,59 @@
 import { useEffect, useRef } from 'react';
 
-const KAKAO_API_KEY = 'YOUR_APP_KEY'; // Replace with your actual Kakao API key
-
 const Map = () => {
 	const mapRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
-		// Load the Kakao Maps script and initialize the map
-		if (window && (window as any).kakao) {
-			const kakao = (window as any).kakao;
+		const initMap = async () => {
+			if (window && (window as any).kakao) {
+				const kakao = (window as any).kakao;
 
-			// Initialize the Kakao Maps library
-			kakao.maps.load(() => {
-				if (mapRef.current) {
-					const map = new kakao.maps.Map(mapRef.current, {
-						center: new kakao.maps.LatLng(37.5665, 126.978), // Default center (Seoul)
-						level: 3, // Zoom level
-					});
+				kakao.maps.load(() => {
+					if (mapRef.current) {
+						// 서울
+						let center = new kakao.maps.LatLng(37.5665, 126.978);
 
-					// You can add markers or other map features here
-					new kakao.maps.Marker({
-						position: new kakao.maps.LatLng(37.5665, 126.978),
-						map: map,
-					});
-				}
-			});
-		}
+						if (navigator.geolocation) {
+							navigator.geolocation.getCurrentPosition(
+								position => {
+									const { latitude, longitude } = position.coords;
+									center = new kakao.maps.LatLng(latitude, longitude);
+
+									// 현재 위치 중앙으로
+									const map = new kakao.maps.Map(mapRef.current, {
+										center: center,
+										level: 3,
+									});
+
+									new kakao.maps.Marker({
+										position: center,
+										map: map,
+									});
+								},
+								error => {
+									console.error('Error getting geolocation:', error);
+
+									// 현재위치 추적 실패하면 서울 기준
+									const map = new kakao.maps.Map(mapRef.current, {
+										center: center,
+										level: 3,
+									});
+								},
+							);
+						} else {
+							console.error('현재위치 인식 실패');
+
+							const map = new kakao.maps.Map(mapRef.current, {
+								center: center,
+								level: 3,
+							});
+						}
+					}
+				});
+			}
+		};
+
+		initMap();
 	}, []);
 
 	return <div ref={mapRef} style={{ width: '100%', height: '100vh' }} />;
