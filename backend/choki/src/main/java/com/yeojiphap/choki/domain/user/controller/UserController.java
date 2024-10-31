60372510e6a97a6cec8b91346054fd14f661285c
JiEung2 <wldmd5007@naver.com>
2024-10-31 17:26:54 +0900

    fix(userController): RequestMapping 추가 및 매개변수에 RequestBody 설정 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/user/controller/UserController.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/user/controller/UserController.java
@@ -6,15 +6,18 @@ import com.yeojiphap.choki.global.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ApiResponse signup(signUpRequest signUpRequest) {
+    public ApiResponse signup(@RequestBody signUpRequest signUpRequest) {
         return ApiResponse.success(HttpStatus.CREATED, userService.signUp(signUpRequest));
     }
 }
