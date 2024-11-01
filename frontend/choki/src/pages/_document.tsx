e6bc5db8744214a56a8a5fcd03c1e7ef34444773
인호현 <inhohyun36@gmail.com>
2024-11-01 10:48:02 +0900

    fix: prettier 에러 해결

--- frontend/choki/src/pages/_document.tsx
+++ frontend/choki/src/pages/_document.tsx
@@ -1,18 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/public/choki512x512.png" />
-      </Head>
-      <body className="antialiased">
-        <div id="root">
-          <Main />
-          <NextScript />
-        </div>
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<link rel="manifest" href="/manifest.json" />
+				<link rel="icon" href="/public/choki512x512.png" />
+			</Head>
+			<body className="antialiased">
+				<div id="root">
+					<Main />
+					<NextScript />
+				</div>
+			</body>
+		</Html>
+	);
 }
