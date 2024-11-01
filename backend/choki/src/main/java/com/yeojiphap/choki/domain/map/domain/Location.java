4d859c16d454c74aa484fd4f5b393314ed81a7dd
JiEung2 <wldmd5007@naver.com>
2024-11-01 17:50:10 +0900

    feat(location): 위도와 경도, 이름을 지니고 있는 location 클래스 추가

---
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/domain/Location.java
@@ -0,0 +1,10 @@
+package com.yeojiphap.choki.domain.map.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Location {
+    private String buildingName;
+    private Double latitude;
+    private Double longitude;
+}
