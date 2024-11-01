6a54ac1f459d8ece3affa6656c3a55122906298a
piilll <spchoi1998@naver.com>
2024-11-01 10:14:25 +0900

    fix: 지도 페이지 코드 리팩토링

--- frontend/choki/src/components/map/Map.tsx
+++ frontend/choki/src/components/map/Map.tsx
@@ -1,69 +1,18 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
+import MapComponent from './MapComponent';
+import UserLocation from './UserLocation';
+import Navigation from './Navigation';
 
 const Map = () => {
-	const mapRef = useRef<HTMLDivElement>(null);
 	const [map, setMap] = useState<any>(null);
-	const [marker, setMarker] = useState<any>(null);
 
-	useEffect(() => {
-		const initMap = async () => {
-			if (window && (window as any).kakao) {
-				const kakao = (window as any).kakao;
-
-				kakao.maps.load(() => {
-					if (mapRef.current) {
-						const initialCenter = new kakao.maps.LatLng(37.5665, 126.978);
-						const mapInstance = new kakao.maps.Map(mapRef.current, {
-							center: initialCenter,
-							level: 3,
-						});
-
-						const customIconSrc = '/choki192x192.png';
-						const customIconSize = new kakao.maps.Size(32, 32);
-						const customIcon = new kakao.maps.MarkerImage(
-							customIconSrc,
-							customIconSize,
-						);
-
-						const initialMarker = new kakao.maps.Marker({
-							position: initialCenter,
-							image: customIcon,
-							map: mapInstance,
-						});
-
-						setMap(mapInstance);
-						setMarker(initialMarker);
-
-						if (navigator.geolocation) {
-							navigator.geolocation.watchPosition(
-								position => {
-									const { latitude, longitude } = position.coords;
-									const newCenter = new kakao.maps.LatLng(latitude, longitude);
-
-									mapInstance.setCenter(newCenter);
-									initialMarker.setPosition(newCenter);
-								},
-								error => {
-									console.error('Error watching position:', error);
-								},
-								{
-									enableHighAccuracy: true,
-									timeout: 5000,
-									maximumAge: 0,
-								},
-							);
-						} else {
-							console.error('Geolocation is not supported by this browser.');
-						}
-					}
-				});
-			}
-		};
-
-		initMap();
-	}, []);
-
-	return <div ref={mapRef} style={{ width: '100%', height: '100vh' }} />;
+	return (
+		<div>
+			<MapComponent onMapLoad={setMap} />
+			{map && <UserLocation map={map} />}
+			<Navigation />
+		</div>
+	);
 };
 
 export default Map;
