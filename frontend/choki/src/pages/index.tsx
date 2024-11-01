adc9ce09b3e475e6adadceeee3273503985c8b5c
김민주 <kminju2000@naver.com>
2024-11-01 17:58:17 +0900

    chore: 구조 테스트

--- frontend/choki/src/pages/index.tsx
+++ frontend/choki/src/pages/index.tsx
@@ -1,8 +1,4 @@
-import Navbar from '@/components/Navbar';
+import BottomNavbar from '@/components/BottomNavbar';
 export default function Home() {
-<<<<<<< HEAD
-	return <Navbar />;
-=======
-	return <>왜 안되는걸까요?</>;
->>>>>>> 9fd68eb3a03937c64bb78e95ec09398d81c0b196
+	return <BottomNavbar />;
 }
---
+++ frontend/choki/src/pages/parents/index.tsx
@@ -0,0 +1,7 @@
+export default function Parents() {
+	return (
+		<>
+			<div>안녕하세요 민주님</div>
+		</>
+	);
+}
