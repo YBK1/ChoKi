74f7a503213c3a85867b706ce90c30b0a14a56ee
JiEung2 <wldmd5007@naver.com>
2024-11-01 15:44:12 +0900

    rename(character): mysql 예약어와 겹치기 때문에 character에서 animal로 수정

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/collected/domain/Collected.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/collected/domain/Collected.java
@@ -1,6 +1,6 @@
 package com.yeojiphap.choki.domain.collected.domain;
 
-import com.yeojiphap.choki.domain.character.domain.Character;
+import com.yeojiphap.choki.domain.character.domain.Animal;
 import com.yeojiphap.choki.domain.user.domain.User;
 import jakarta.persistence.*;
 import lombok.*;
@@ -21,6 +21,6 @@ public class Collected {
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "character_id", nullable = false)
-    private Character character;
+    @JoinColumn(name = "animal_id", nullable = false)
+    private Animal animal;
 }
