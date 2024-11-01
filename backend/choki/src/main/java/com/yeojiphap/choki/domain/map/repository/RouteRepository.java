3f473c3dbb11d0f72e9e058d0613781be00ef637
JiEung2 <wldmd5007@naver.com>
2024-11-01 17:50:29 +0900

    feat(routeRepository): Route repository 추가

---
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/repository/RouteRepository.java
@@ -0,0 +1,6 @@
+package com.yeojiphap.choki.domain.map.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RouteRepository extends MongoRepository<RouteRepository, Long> {
+}
