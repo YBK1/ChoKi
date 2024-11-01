53de1582c2b44b0014341f6aa3f256d015875547
piilll <spchoi1998@naver.com>
2024-11-01 10:38:16 +0900

    fix: 지도 구조 리팩토링

--- frontend/choki/src/pages/map/index.tsx
+++ frontend/choki/src/pages/map/index.tsx
@@ -1,14 +1,22 @@
+// pages/map.tsx
+import { useState } from 'react';
 import dynamic from 'next/dynamic';
 
-// Dynamically import the Map component to ensure it runs on the client side
 const DynamicMap = dynamic(() => import('@/components/map/Map'), {
 	ssr: false,
 });
+const DynamicUserLocation = dynamic(
+	() => import('@/components/map/UserLocation'),
+	{ ssr: false },
+);
 
 const MapPage = () => {
+	const [map, setMap] = useState<any>(null);
+
 	return (
 		<div style={{ height: '100vh', width: '100%' }}>
-			<DynamicMap />
+			<DynamicMap onMapLoad={setMap} />
+			{map && <DynamicUserLocation map={map} />}
 		</div>
 	);
 };
