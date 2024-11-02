44e13817c92a56799cec8ae48fe15e71de306263
인호현 <inhohyun36@gmail.com>
2024-11-02 16:04:36 +0900

    test: 인풋창 테스트코드 삭제

--- frontend/choki/src/pages/index.tsx
+++ frontend/choki/src/pages/index.tsx
@@ -1,13 +1,3 @@
-import { useState } from 'react';
-import CommonInput from '@/components/Common/Input';
 export default function Home() {
-	const [value, setValue] = useState('');
-
-	return (
-		<CommonInput
-			placeholder="텍스트를 입력하세요"
-			value={value}
-			onChange={e => setValue(e.target.value)}
-		/>
-	);
+	return <>Home</>;
 }
