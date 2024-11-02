63e9c0da8d94ceaf273e91074ded0e89cb68014b
MINJOO-KIM <gkminju2000@gmail.com>
2024-11-02 18:59:55 +0900

    feat: 부모 시작페이지 - 아이 선택 레이아웃 구현

--- frontend/choki/src/pages/parents/index.tsx
+++ frontend/choki/src/pages/parents/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import dog_character from '@/assets/icons/dog_character.svg';
 import code_information from '@/assets/icons/cod-information.svg';
+import child_profile from '@/assets/icons/child_profile.svg';
 
 export default function index() {
 	return (
@@ -19,6 +20,30 @@ export default function index() {
 				/>
 				<div className="absolute top-10 right-2">
 					<Image src={code_information} alt="code_information" />
+				</div>
+			</div>
+
+			{/* 내용 */}
+			<div className="flex flex-col justify-center items-center gap-4 m-8">
+				{/* 아이 선택 */}
+				<div className="w-[350px] h-[270px] bg-white rounded-3xl shadow-sm border-4 border-light_yellow_side">
+					<h2 className="text-lg font-bold mt-8 ml-7 mb-4">
+						오늘은 어떤 아이에게
+						<br />
+						미션을 부여하실건가요?
+					</h2>
+					<div className="flex justify-center gap-16">
+						<div className="flex flex-col items-center">
+							<Image src={child_profile} alt="child_profile" />
+							<p className="text-sm mt-2">여준이는 심부름왕</p>
+							<p className="text-sm font-bold mt-1">Lv.10</p>
+						</div>
+						<div className="flex flex-col items-center">
+							<Image src={child_profile} alt="child_profile" />
+							<p className="text-sm mt-2">장덕동 심부름왕</p>
+							<p className="text-sm font-bold mt-1">Lv.5</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
