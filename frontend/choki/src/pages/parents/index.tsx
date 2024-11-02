29c94d831cdbb26385e2ee7f78d28a2e100157e5
MINJOO-KIM <gkminju2000@gmail.com>
2024-11-02 19:37:47 +0900

    feat: 부모 시작페이지 - 경로 등록 레이아웃 구현

--- frontend/choki/src/pages/parents/index.tsx
+++ frontend/choki/src/pages/parents/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import dog_character from '@/assets/icons/dog_character.svg';
 import code_information from '@/assets/icons/cod-information.svg';
 import child_profile from '@/assets/icons/child_profile.svg';
+import map_icon_blurry from '@/assets/icons/map_icon_blurry.svg';
 
 export default function index() {
 	return (
@@ -24,9 +25,9 @@ export default function index() {
 			</div>
 
 			{/* 내용 */}
-			<div className="flex flex-col justify-center items-center gap-4 m-8">
+			<div className="flex flex-col justify-center items-center gap-4 mt-11">
 				{/* 아이 선택 */}
-				<div className="w-[350px] h-[270px] bg-white rounded-3xl shadow-sm border-4 border-light_yellow_side">
+				<div className="w-[350px] h-[270px] bg-white rounded-3xl shadow-sm border-4 border-light_yellow_side mb-2">
 					<h2 className="text-lg font-bold mt-8 ml-7 mb-4">
 						오늘은 어떤 아이에게
 						<br />
@@ -43,6 +44,25 @@ export default function index() {
 							<p className="text-sm mt-2">장덕동 심부름왕</p>
 							<p className="text-sm font-bold mt-1">Lv.5</p>
 						</div>
+					</div>
+				</div>
+
+				{/* 경로 등록하기 */}
+				<div className="relative w-[350px] h-[180px] bg-white p-6 rounded-3xl shadow-sm border-4 border-light_blue_side">
+					<h2 className="text-xl font-bold mb-2 mt-4 relative z-10">
+						경로 등록하기
+					</h2>
+					<p className="text-md text-gray-600 relative z-10">
+						아이에게 심부름을 시킬
+						<br />
+						경로를 미리 등록해주세요!
+					</p>
+					<div className="absolute right-0 bottom-0">
+						<Image
+							src={map_icon_blurry}
+							alt="map_icon_blurry"
+							className="opacity-50" // 선택적: 배경이미지처럼 보이도록 투명도 추가
+						/>
 					</div>
 				</div>
 			</div>
