019293c498780e5c1da1b578d03df8acc489be25
JiEung2 <181757@jnu.ac.kr>
2024-11-05 01:35:55 +0900

    feat(userSuccessMessage): 가족 생성 성공 메시지 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/user/message/UserSuccessMessage.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/user/message/UserSuccessMessage.java
@@ -1,7 +1,8 @@
 package com.yeojiphap.choki.domain.user.message;
 
 public enum UserSuccessMessage {
-    SIGN_UP_SUCCESS("회원가입 성공"),;
+    SIGN_UP_SUCCESS("회원가입 성공"),
+    FAMILY_CREATION_SUCCESS("가족 생성 성공");
     private final String message;
 
     UserSuccessMessage(String message) {
