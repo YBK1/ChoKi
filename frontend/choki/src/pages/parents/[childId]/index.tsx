173bed50f10733ad5827c875312c38d144351be4
인호현 <inhohyun36@gmail.com>
2024-11-04 10:41:20 +0900

    fix: build 에러 해결

--- frontend/choki/src/pages/parents/[childId]/index.tsx
+++ frontend/choki/src/pages/parents/[childId]/index.tsx
@@ -2,7 +2,7 @@ export default function index() {
 	return (
 		<>
 			<div className="flex flex-col w-full max-w-md mx-auto bg-light_yellow background min-h-screen"></div>
-            {/* 알림 아이콘: 누르면 parents/notification으로 이동 */}
+			{/* 알림 아이콘: 누르면 parents/notification으로 이동 */}
 			{/* 안내문구 */}
 			{/* 아이정보 */}
 			{/* 심부름 목록: 추가 버튼 누르면 모달 생성, 백엔드에게 받아온 진행중인 미션 목록 띄워주기 */}
