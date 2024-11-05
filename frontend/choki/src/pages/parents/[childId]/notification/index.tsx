3f37b1430b74c95f658288b0b995cc1d8b7d62bd
김민주 <kminju2000@naver.com>
2024-11-05 14:51:09 +0900

    feat: 부모 메인 알림페이지 - 알림 목록 레이아웃 구현

--- frontend/choki/src/pages/parents/[childId]/notification/index.tsx
+++ frontend/choki/src/pages/parents/[childId]/notification/index.tsx
@@ -1,24 +1,78 @@
-import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 import previous_icon from '@/assets/icons/previous.svg';
+import { MISSION_IMAGES } from '@/constants/mission';
+import type { NotificationResponse } from '@/types/http';
+import right_arrow from '@/assets/icons/right_arrow.svg';
 
 export default function NotificationPage() {
 	const router = useRouter();
+
+	const notifications: NotificationResponse[] = [
+		{
+			childId: 2,
+			content: '아이가 장보기를 시작했어요!',
+			type: 'SHOP',
+			id: 1,
+			time: '24.07.01 08:02',
+		},
+		{
+			childId: 2,
+			content: '아이가 재활용 미션을 완료했어요! 미션 내용을 확인해보세요!',
+			type: 'RECYCLE',
+			id: 2,
+			time: '24.07.01 08:03',
+		},
+		{
+			childId: 2,
+			content: '아이가 추가 미션을 완료했어요! 미션 내용을 확인해보세요!',
+			type: 'EXTRA_MISSION',
+			id: 3,
+			time: '24.07.01 08:02',
+		},
+	];
 
 	const handleGoBack = () => {
 		router.back();
 	};
 
 	return (
-		<>
-			<div className="flex flex-col w-full max-w-md mx-auto bg-light_yellow background min-h-screen">
-				<Image
-					src={previous_icon}
-					alt="previous_icon"
-					className="w-12 h-12 m-4 cursor-pointer" // cursor-pointer 추가
-					onClick={handleGoBack}
-				/>
+		<div className="flex flex-col w-full max-w-md mx-auto bg-light_yellow min-h-screen">
+			<Image
+				src={previous_icon}
+				alt="previous_icon"
+				className="w-12 h-12 m-4 cursor-pointer"
+				onClick={handleGoBack}
+			/>
+
+			{/* 알림목록 */}
+			<div className="flex flex-col gap-4 p-4">
+				{notifications.map(notification => (
+					<div
+						key={notification.id}
+						className="flex items-center p-4 shadow-sm"
+					>
+						<div className="relative flex items-center justify-center">
+							<Image
+								src={MISSION_IMAGES[notification.type]}
+								alt={notification.type}
+								width={60}
+								height={60}
+								className="object-contain"
+							/>
+						</div>
+
+						<div className="flex flex-col ml-4 flex-grow">
+							<span className="text-base">{notification.content}</span>
+							<span className="text-sm text-gray-400">{notification.time}</span>
+						</div>
+
+						<Image src={right_arrow} alt="detail" width={24} height={24} />
+					</div>
+				))}
 			</div>
-		</>
+
+			{/* 하단 네비게이션... */}
+		</div>
 	);
 }
