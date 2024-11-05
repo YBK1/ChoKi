c8d9cd3777ff76db38c34870595f0e60b153a3c9
MINJOO-KIM <gkminju2000@gmail.com>
2024-11-06 06:34:12 +0900

    fix: 불필요한 export 제거

--- frontend/choki/src/types/http.d.ts
+++ frontend/choki/src/types/http.d.ts
@@ -1,12 +1,12 @@
 //Response 뒤에 붙이기
 
-// export interface BaseResponse<T> {
+// interface BaseResponse<T> {
 // 	status: number;
 // 	message: string;
 // 	data: T;
 // }
 
-export interface NotificationResponse {
+interface NotificationResponse {
 	childId: number;
 	content: string;
 	type: MissionType;
