4b69a0b5483e8f117eb33066fa580378e6906c34
JiEung2 <181757@jnu.ac.kr>
2024-11-05 21:44:26 +0900

    fix(location): location 클래스 record로 변경
    - location은 document, entity가 아닌 단순 데이터의 성격을 띄므로 record로 변경

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/domain/Location.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/domain/Location.java
@@ -1,14 +1,4 @@
 package com.yeojiphap.choki.domain.map.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-@AllArgsConstructor
-public class Location {
-    private String buildingName;
-    private Double latitude;
-    private Double longitude;
+public record Location(String buildingName, Double latitude, Double longitude) {
 }
