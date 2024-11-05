8df42afc583e31f120a1676a2c6b9a7a9a7b0cde
JiEung2 <wldmd5007@naver.com>
2024-11-05 17:54:47 +0900

    feat(routeRequest): 경로 저장 요청 dto 생성

---
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/dto/request/RouteRequest.java
@@ -0,0 +1,8 @@
+package com.yeojiphap.choki.domain.map.dto.request;
+
+import com.yeojiphap.choki.domain.map.domain.Location;
+
+import java.util.List;
+
+public record RouteRequest(Location startPoint, Location destination, List<Location>routes) {
+}
