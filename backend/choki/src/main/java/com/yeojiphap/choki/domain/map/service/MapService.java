3e5bdefe70276bc989c450b2bcff402088e7dd5c
JiEung2 <wldmd5007@naver.com>
2024-11-05 17:55:17 +0900

    feat(mapService): 예습 경로를 저장하는 서비스 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/service/MapService.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/service/MapService.java
@@ -1,4 +1,30 @@
 package com.yeojiphap.choki.domain.map.service;
 
+import com.yeojiphap.choki.domain.map.domain.GuidedRoute;
+import com.yeojiphap.choki.domain.map.dto.request.RouteRequest;
+import com.yeojiphap.choki.domain.map.repository.GuidedRouteRepository;
+import com.yeojiphap.choki.global.auth.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.yeojiphap.choki.domain.map.message.MapSuccessMessage.*;
+
+@Service
+@RequiredArgsConstructor
 public class MapService {
+
+    private final GuidedRouteRepository guidedRouteRepository;
+
+    public String saveGuidedRoute(RouteRequest request) {
+        GuidedRoute guidedRoute = GuidedRoute.builder()
+                .userId(SecurityUtil.getCurrentUserId())
+                .startPoint(request.startPoint())
+                .destination(request.destination())
+                .routes(request.routes())
+                .build();
+
+        guidedRouteRepository.save(guidedRoute);
+        return GUIDED_ROUTE_SAVE_SUCCESS.getMessage();
+    }
+
 }
