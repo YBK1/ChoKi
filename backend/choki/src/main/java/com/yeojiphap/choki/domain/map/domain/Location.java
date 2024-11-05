6d43a4c577675977cb7592a2b6241c69430bdca2
JiEung2 <181757@jnu.ac.kr>
2024-11-05 20:39:44 +0900

    feat(location): Builder 패턴 적용

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/domain/Location.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/domain/Location.java
@@ -1,8 +1,12 @@
 package com.yeojiphap.choki.domain.map.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
+@AllArgsConstructor
 public class Location {
     private String buildingName;
     private Double latitude;
