89ca760d88172efec2351602e6e0cef6485cac0a
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-11-06 14:22:03 +0900

    fix(family): 누락된 Repository, Service 로직 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/family/repository/FamilyRepository.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/family/repository/FamilyRepository.java
@@ -1,10 +1,17 @@
 package com.yeojiphap.choki.domain.family.repository;
 
 import com.yeojiphap.choki.domain.family.domain.Family;
+import com.yeojiphap.choki.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FamilyRepository extends JpaRepository<Family, Long> {
     Optional<Family> findByUsers_UserId(String userId);
+
+    @Query("SELECT u FROM User u WHERE u.family.id = :familyId AND u.role = 'CHILD'")
+    public List<User> getChildren(@Param("familyId") Long familyId);
 }
--- backend/choki/src/main/java/com/yeojiphap/choki/domain/family/service/FamilyService.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/family/service/FamilyService.java
@@ -1,14 +1,19 @@
 package com.yeojiphap.choki.domain.family.service;
 
 import com.yeojiphap.choki.domain.family.domain.Family;
+import com.yeojiphap.choki.domain.family.dto.ChildResponseDto;
+import com.yeojiphap.choki.domain.user.domain.Role;
 import com.yeojiphap.choki.domain.user.domain.User;
 import com.yeojiphap.choki.domain.family.dto.InviteCodeResponse;
+import com.yeojiphap.choki.domain.user.exception.InvalidUserRoleException;
 import com.yeojiphap.choki.domain.user.exception.UserNotFoundException;
 import com.yeojiphap.choki.domain.family.repository.FamilyRepository;
 import com.yeojiphap.choki.domain.user.repository.UserRepository;
 import com.yeojiphap.choki.global.auth.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -37,5 +42,23 @@ public class FamilyService {
     private User findCurrentUser() {
         return userRepository.findByUserId(SecurityUtil.getCurrentUserId())
                 .orElseThrow(UserNotFoundException::new);
+    }
+
+    public List<ChildResponseDto> getChildInfoByFamilyId() {
+        User user = findCurrentUser();
+
+        if (user == null) {
+            throw new UserNotFoundException();
+        }
+
+        if (!user.getRole().equals(Role.PARENT)) {
+            throw new InvalidUserRoleException();
+        }
+
+        List<User> children = familyRepository.getChildren(user.getFamily().getId());
+
+        return children.stream()
+                .map(ChildResponseDto::from)
+                .toList();
     }
 }
