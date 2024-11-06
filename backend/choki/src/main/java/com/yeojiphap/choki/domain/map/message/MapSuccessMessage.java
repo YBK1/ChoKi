47cc9d9f3fb58dce5c478f12ce1a36d9b9a0bb6a
JiEung2 <wldmd5007@naver.com>
2024-11-06 15:45:01 +0900

    feat(mapSuccessMessage): 가이드 경로 목록 조회 성공 메시지 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/message/MapSuccessMessage.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/message/MapSuccessMessage.java
@@ -1,7 +1,8 @@
 package com.yeojiphap.choki.domain.map.message;
 
 public enum MapSuccessMessage {
-    GUIDED_ROUTE_SAVE_SUCCESS("예습 경로 저장 성공"),;
+    GUIDED_ROUTE_SAVE_SUCCESS("예습 경로 저장 성공"),
+    GUIDED_ROUTES_SEARCH_SUCCESS("가이드 경로 목록 조회 성공");
     private final String message;
 
     MapSuccessMessage(String message) {
