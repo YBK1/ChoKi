ac1a3c42e1b21e1d4e6922c8c5bc7a28c2fe6bc5
JiEung2 <181757@jnu.ac.kr>
2024-11-06 23:58:41 +0900

    feat(userSuccessMessage): 사용 가능한 아이디일 때의 메시지 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/user/message/UserSuccessMessage.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/user/message/UserSuccessMessage.java
@@ -2,6 +2,7 @@ package com.yeojiphap.choki.domain.user.message;
 
 public enum UserSuccessMessage {
     SIGN_UP_SUCCESS("회원가입 성공"),
+    USER_ID_VALIDATION_SUCCESS("사용 가능한 아이디입니다."),
     ;
 
     private final String message;
