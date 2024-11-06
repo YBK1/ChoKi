d8fea34f8e88ac38f48c543eb8b5716dd4999ef6
JiEung2 <wldmd5007@naver.com>
2024-11-06 14:59:32 +0900

    feat(familyController): 초대 코드를 통해 가족을 등록하는 컨트롤러 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/family/controller/FamilyController.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/family/controller/FamilyController.java
@@ -1,13 +1,11 @@
 package com.yeojiphap.choki.domain.family.controller;
 
+import com.yeojiphap.choki.domain.family.dto.InviteCodeDto;
 import com.yeojiphap.choki.domain.family.service.FamilyService;
 import com.yeojiphap.choki.global.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.yeojiphap.choki.domain.family.message.FamilySuccessMessage.FAMILY_CREATION_SUCCESS;
 import static com.yeojiphap.choki.domain.family.message.FamilySuccessMessage.GET_CHILD_INFO_SUCCESS;
@@ -31,5 +29,10 @@ public class FamilyController implements SpringDocFamilyController{
     @GetMapping("/info")
     public ApiResponse getFamily() {
         return ApiResponse.success(HttpStatus.OK, familyService.getChildInfoByFamilyId(), GET_CHILD_INFO_SUCCESS.getMessage());
+    }
+
+    @PostMapping("/invite-code/accept")
+    public ApiResponse acceptInviteCode(@RequestBody InviteCodeDto request) {
+        return ApiResponse.success(HttpStatus.ACCEPTED, familyService.acceptInviteCode(request));
     }
 }
