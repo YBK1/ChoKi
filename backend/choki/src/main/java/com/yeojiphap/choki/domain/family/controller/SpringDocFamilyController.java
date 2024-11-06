51490a64d8b5730b6de97b7121a0a118c7168051
JiEung2 <wldmd5007@naver.com>
2024-11-06 15:04:07 +0900

    feat(familyController): 초대 코드를 통해 가족을 등록하는 swagger 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/family/controller/SpringDocFamilyController.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/family/controller/SpringDocFamilyController.java
@@ -1,11 +1,13 @@
 package com.yeojiphap.choki.domain.family.controller;
 
+import com.yeojiphap.choki.domain.family.dto.InviteCodeDto;
 import com.yeojiphap.choki.global.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "가족 컨트롤러", description = "가족 및 초대코드를 생성, 조회하는 등 가족을 관리하는 클래스")
 public interface SpringDocFamilyController {
@@ -56,4 +58,26 @@ public interface SpringDocFamilyController {
             ),
     })
     public ApiResponse getInviteCode();
+
+    @Operation(
+            summary = "초대 코드 입력, 가족 등록",
+            description = "초대 코드를 입력하여 가족에 가입합니다."
+    )
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "가족 등록 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(value = """
+                                    {
+                                      "status": 200,
+                                      "message": 가족 등록 성공,
+                                      "data": null
+                                    }"""
+                            )
+                    )
+            ),
+    })
+    public ApiResponse acceptInviteCode(@RequestBody InviteCodeDto request);
 }
