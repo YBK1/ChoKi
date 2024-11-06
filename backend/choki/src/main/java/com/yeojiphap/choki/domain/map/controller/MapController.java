691c84367a5bc5331261dd9ea64f2ebe7bcce2a8
JiEung2 <wldmd5007@naver.com>
2024-11-06 15:48:01 +0900

    feat(guidedRoutes): 예습 경로 목록을 조회하는 컨트롤러 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/controller/MapController.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/controller/MapController.java
@@ -5,10 +5,9 @@ import com.yeojiphap.choki.domain.map.service.MapService;
 import com.yeojiphap.choki.global.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.yeojiphap.choki.domain.map.message.MapSuccessMessage.GUIDED_ROUTES_SEARCH_SUCCESS;
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +18,10 @@ public class MapController implements SpringDocMapController{
     @PostMapping("/route")
     public ApiResponse saveGuidedRoute(@RequestBody RouteRequest request){
         return ApiResponse.success(HttpStatus.CREATED, mapService.saveGuidedRoute(request));
+    }
+
+    @GetMapping("/route/guide")
+    public ApiResponse getGuidedRoute(){
+        return ApiResponse.success(HttpStatus.OK, mapService.getGuidedRoutes(), GUIDED_ROUTES_SEARCH_SUCCESS.getMessage());
     }
 }
