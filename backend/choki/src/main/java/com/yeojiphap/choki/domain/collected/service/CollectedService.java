64c9b0b820c3afcd407ef7af1ef23891d92d6dde
JiEung2 <181757@jnu.ac.kr>
2024-11-08 00:18:51 +0900

    feat(collectedService): 랜덤으로 동물을 뽑기 위한 기능 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/collected/service/CollectedService.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/collected/service/CollectedService.java
@@ -5,7 +5,9 @@ import com.yeojiphap.choki.domain.character.dto.AnimalDto;
 import com.yeojiphap.choki.domain.character.dto.AnimalListDto;
 import com.yeojiphap.choki.domain.character.exception.AnimalNotFoundException;
 import com.yeojiphap.choki.domain.character.repository.AnimalRepository;
+import com.yeojiphap.choki.domain.collected.domain.AnimalGrade;
 import com.yeojiphap.choki.domain.collected.domain.Collected;
+import com.yeojiphap.choki.domain.collected.dto.UnownedAnimalsDto;
 import com.yeojiphap.choki.domain.collected.exception.AnimalAlreadyExistException;
 import com.yeojiphap.choki.domain.collected.exception.AnimalNotOwnedException;
 import com.yeojiphap.choki.domain.collected.repository.CollectedRepository;
@@ -18,7 +20,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Random;
 
+import static com.yeojiphap.choki.domain.collected.domain.AnimalGrade.*;
 import static com.yeojiphap.choki.domain.collected.message.CollectedSuccessMessage.*;
 
 @Service
@@ -27,6 +31,7 @@ public class CollectedService {
     private final CollectedRepository collectedRepository;
     private final UserRepository userRepository;
     private final AnimalRepository animalRepository;
+    private static final Random random = new Random();
 
     @Transactional
     public void addBaseAnimalToUser(Long userId, Long animalId) {
@@ -63,6 +68,57 @@ public class CollectedService {
 
         user.updateMainAnimal(animalId);
         return MAIN_ANIMAL_UPDATE_SUCCESS.getMessage();
+    }
+
+    public Long drawRandomAnimal() {
+        UnownedAnimalsDto unownedAnimalsDtos = getUnownedAnimals();
+        return pickUnownedAnimal(unownedAnimalsDtos.unownedUnique(), unownedAnimalsDtos.unownedRare(), unownedAnimalsDtos.unownedCommon());
+    }
+
+    private Long pickUnownedAnimal(List<Long> unownedUnique, List<Long> unownedRare, List<Long> unownedCommon) {
+        AnimalGrade grade = pickGrade();
+        List<Long> characters;
+
+        switch (grade) {
+            case UNIQUE -> characters = unownedUnique;
+            case RARE -> characters = unownedRare;
+            default -> characters = unownedCommon;
+        }
+
+        if (characters.isEmpty()) {
+            characters = unownedCommon.isEmpty() ? unownedRare : unownedCommon;
+        }
+
+        return characters.get(random.nextInt(characters.size()));
+    }
+
+    private UnownedAnimalsDto getUnownedAnimals() {
+        User user = findCurrentUser();
+        List<Long> collectedAnimalIds = collectedRepository.findAnimalIdsByUserId(user.getId());
+
+        List<Long> unownedUnique = UNIQUE.getCharacterIds().stream()
+                .filter(id -> !collectedAnimalIds.contains(id))
+                .toList();
+        List<Long> unownedRare = RARE.getCharacterIds().stream()
+                .filter(id -> !collectedAnimalIds.contains(id))
+                .toList();
+        List<Long> unownedCommon = COMMON.getCharacterIds().stream()
+                .filter(id -> !collectedAnimalIds.contains(id))
+                .toList();
+
+        return new UnownedAnimalsDto(unownedUnique, unownedRare, unownedCommon);
+    }
+
+    private AnimalGrade pickGrade() {
+        int randomValue = random.nextInt(100) + 1;
+
+        if (randomValue <= 10) {
+            return UNIQUE;
+        } else if (randomValue <= 40) {
+            return RARE;
+        } else {
+            return COMMON;
+        }
     }
 
     private void validateAnimalOwnership(Long userId, Long animalId) {
