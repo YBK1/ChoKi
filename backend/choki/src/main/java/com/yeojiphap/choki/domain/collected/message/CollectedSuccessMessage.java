ab23ac0ffe8a1406fd23b0167fd94c0a9e6cf0a4
JiEung2 <181757@jnu.ac.kr>
2024-11-07 21:10:09 +0900

    feat(collectedSuccessMessage): 메인 캐릭터 변경 성공 메시지 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/collected/message/CollectedSuccessMessage.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/collected/message/CollectedSuccessMessage.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum CollectedSuccessMessage {
     COLLECTED_REGIST_SUCCESS("캐릭터 등록 성공"),
-    COLLECTED_SEARCH_MESSAGE("보유 캐릭터 조회 성공");
+    COLLECTED_SEARCH_MESSAGE("보유 캐릭터 조회 성공"),
+    MAIN_ANIMAL_UPDATE_SUCCESS("메인 캐릭터 변경 성공");
 
     private final String message;
 
