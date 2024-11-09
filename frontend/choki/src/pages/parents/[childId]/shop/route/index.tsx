fc4de1fb177d4f6b750f9851e1647967a724050d
piilll <spchoi1998@naver.com>
2024-11-10 03:09:01 +0900

    feat : 부모가 보는 아이 지도 화면에 경로 선 그려지도록 구현

--- frontend/choki/src/pages/parents/[childId]/shop/route/index.tsx
+++ frontend/choki/src/pages/parents/[childId]/shop/route/index.tsx
@@ -2,6 +2,18 @@ import BottomNavbar from '@/components/Common/Navbar/BottomNavbar';
 import Map from '@/components/map/Map';
 
 export default function ChildIsShoppingPage() {
+	const route = [
+		{ latitude: 35.1651676, longitude: 126.8788277 },
+		{ latitude: 35.1655676, longitude: 126.8789277 },
+		{ latitude: 35.1657676, longitude: 126.8786277 },
+		{ latitude: 35.1652676, longitude: 126.8790277 },
+		{ latitude: 35.1649676, longitude: 126.8785277 },
+		{ latitude: 35.1653676, longitude: 126.8787277 },
+		{ latitude: 35.1648676, longitude: 126.8784277 },
+		{ latitude: 35.1654676, longitude: 126.8783277 },
+		{ latitude: 35.1647676, longitude: 126.8791277 },
+		{ latitude: 35.1658676, longitude: 126.8782277 },
+	];
 	return (
 		<div className="relative min-h-screen bg-light_yellow flex flex-col items-center justify-center">
 			<div
@@ -26,11 +38,11 @@ export default function ChildIsShoppingPage() {
 						showPreviousButton={true}
 						showRouteRecorder={false}
 						showChildNavBar={false}
+						route={route}
 					/>
 				</div>
 			</div>
 
-			{/* Bottom Navigation */}
 			<BottomNavbar />
 		</div>
 	);
