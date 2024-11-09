e8af34763fa4b81b3030a930c88eddcc02b14f2d
MINJOO-KIM <gkminju2000@gmail.com>
2024-11-10 02:14:40 +0900

    feat: jotai UserState 기본 설정

--- frontend/choki/src/atoms/userAtom.ts
+++ frontend/choki/src/atoms/userAtom.ts
@@ -1,8 +1,11 @@
 import { atom } from 'jotai';
 
-//아래는 예제 코드입니다
-export const userAtom = atom({
-	id: '',
-	name: '',
-	isLoggedIn: false,
+interface UserState {
+	userId: number;
+	username: string;
+}
+
+export const userAtom = atom<UserState>({
+	userId: 0,
+	username: '',
 });
