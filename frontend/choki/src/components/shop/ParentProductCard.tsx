73facb69aa0a30bda840922407438351103280d9
MINJOO-KIM <gkminju2000@gmail.com>
2024-11-13 02:30:41 +0900

    feat:  부모 실시간 장바구니 컴포넌트 (ParentProductCard) props 추가 및 key 명 수정

--- frontend/choki/src/components/shop/ParentProductCard.tsx
+++ frontend/choki/src/components/shop/ParentProductCard.tsx
@@ -3,67 +3,22 @@ import Image from 'next/image';
 import MiniWarning from '@/assets/icons/mini_warning.svg';
 
 const ParentProductCard: React.FC<ParentShoppingCardProps> = ({
-	role,
 	ParentsShoppingItem,
 	ChildrenShoppingItem,
+	showWarning,
+	emptyMessage,
 }) => {
 	return (
 		<div className="relative flex items-stretch px-8 py-4 border bg-white border-gray-200 rounded-2xl max-w-xl mx-auto shadow-md">
-			{/* Divider */}
 			<div className="absolute top-4 bottom-4 left-1/2 transform -translate-x-1/2 w-px bg-gray-200"></div>
 
 			{/* Left Section (부모 아이템) */}
 			<div className="flex flex-col items-center w-1/2 pr-6 h-full justify-center">
-				<div className="w-20 h-20 mb-0 flex items-center justify-center">
-					<Image
-						src={ParentsShoppingItem.image || '/icons/default_image.svg'}
-						alt="상품 이미지"
-						layout="fixed"
-						width={64}
-						height={96}
-					/>
-				</div>
-				<p className="text-gray-900 font-medium text-lg">
-					{ParentsShoppingItem.title}
-				</p>
-				<p className="text-gray-500 text-sm">
-					수량:{' '}
-					<span className="font-semibold">{ParentsShoppingItem.count}개</span>
-				</p>
-			</div>
-
-			{/* Right Section (자녀 아이템) */}
-			<div className="flex flex-col items-center w-1/2 pl-6 h-full justify-center relative">
-				{/* WasteBasket 아이콘 (CHILD) 또는 MiniWarning 아이콘 (PARENTS) */}
-				{role === 'PARENTS' && (
-					<div className="absolute top-1 -right-4 transform -translate-y-1/4 cursor-pointer">
-						<Image
-							src={MiniWarning}
-							alt="경고 아이콘"
-							width={30}
-							height={30}
-							onClick={() => console.log('경고 아이콘 클릭')}
-						/>
-					</div>
-				)}
-
-				{/* CHILD일 때 상품이 없을 경우 카메라 아이콘 표시 */}
-				{role === 'CHILD' && !ChildrenShoppingItem ? (
-					<div className="w-12 h-12 mb-2 flex items-center justify-center">
-						<Image
-							src="/icons/camera_icon.svg"
-							alt="카메라 아이콘"
-							layout="fixed"
-							width={48}
-							height={48}
-						/>
-					</div>
-				) : (
-					// 자녀 아이템 정보 표시
+				{ParentsShoppingItem.productName ? (
 					<>
 						<div className="w-20 h-20 mb-0 flex items-center justify-center">
 							<Image
-								src={ChildrenShoppingItem?.image || '/icons/default_image.svg'}
+								src={ParentsShoppingItem.image || '/icons/default_image.svg'}
 								alt="상품 이미지"
 								layout="fixed"
 								width={64}
@@ -71,15 +26,51 @@ const ParentProductCard: React.FC<ParentShoppingCardProps> = ({
 							/>
 						</div>
 						<p className="text-gray-900 font-medium text-lg">
-							{ChildrenShoppingItem?.title}
+							{ParentsShoppingItem.productName}
 						</p>
 						<p className="text-gray-500 text-sm">
 							수량:{' '}
 							<span className="font-semibold">
-								{ChildrenShoppingItem?.count}개
+								{ParentsShoppingItem.quantity}개
 							</span>
 						</p>
 					</>
+				) : (
+					<p className="text-gray-500">{emptyMessage}</p>
+				)}
+			</div>
+
+			{/* Right Section (자녀 아이템) */}
+			<div className="flex flex-col items-center w-1/2 pl-6 h-full justify-center relative">
+				{showWarning && (
+					<div className="absolute top-1 -right-4 transform -translate-y-1/4 cursor-pointer">
+						<Image src={MiniWarning} alt="경고 아이콘" width={30} height={30} />
+					</div>
+				)}
+
+				{ChildrenShoppingItem ? (
+					<>
+						<div className="w-20 h-20 mb-0 flex items-center justify-center">
+							<Image
+								src={ChildrenShoppingItem.image || '/icons/default_image.svg'}
+								alt="상품 이미지"
+								layout="fixed"
+								width={64}
+								height={96}
+							/>
+						</div>
+						<p className="text-gray-900 font-medium text-lg">
+							{ChildrenShoppingItem.productName}
+						</p>
+						<p className="text-gray-500 text-sm">
+							수량:{' '}
+							<span className="font-semibold">
+								{ChildrenShoppingItem.quantity}개
+							</span>
+						</p>
+					</>
+				) : (
+					<p className="text-gray-500">{emptyMessage}</p>
 				)}
 			</div>
 		</div>
