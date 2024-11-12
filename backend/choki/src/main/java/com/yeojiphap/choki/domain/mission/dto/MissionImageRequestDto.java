c03980d77ee677818d796ef4e71fc3cc2942f851
김영빈 <kyb031700@naver.com>
2024-11-12 21:30:29 +0900

    feat: 미션 이미지 추가 구현

---
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/mission/dto/MissionImageRequestDto.java
@@ -0,0 +1,10 @@
+package com.yeojiphap.choki.domain.mission.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MissionImageRequestDto {
+	private String missionId;
+}
