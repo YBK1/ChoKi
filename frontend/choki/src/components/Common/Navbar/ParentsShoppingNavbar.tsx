5b69f375cec22b297cd8a37b255e8daf3e87a06d
김민주 <kminju2000@naver.com>
2024-11-13 16:44:34 +0900

    fix: href 절대경로 수정 및 요소 css 수정

--- frontend/choki/src/components/Common/Navbar/ParentsShoppingNavbar.tsx
+++ frontend/choki/src/components/Common/Navbar/ParentsShoppingNavbar.tsx
@@ -3,30 +3,33 @@ import Link from 'next/link';
 
 interface ParentsShoppingrNavbarProps {
 	childId: string;
+	missionId: string;
 }
 
 const ParentsShoppingNavbar: React.FC<ParentsShoppingrNavbarProps> = ({
 	childId,
+	missionId,
 }) => {
 	return (
 		<>
-			<nav className="fixed top-5 z-100 w-full left-[50%] transform -translate-x-1/2">
-				<div className="h-full flex justify-center items-center gap-5">
+			<nav className="fixed top-6 z-100 w-full left-[43%] transform -translate-x-1/2">
+				<div className="h-full flex justify-center items-center gap-8">
 					{/* 나가기 버튼 */}
 					<Link href={`/parents/${childId}`}>
 						<div className="flex mt-2">
 							<Image
 								src="/icons/previous_nav.svg"
 								alt="back_icon"
-								width={65}
-								height={65}
+								width={42}
+								height={42}
+								className="shadow-md rounded-md"
 							/>
 						</div>
 					</Link>
 
 					{/* 경로 및 장바구니 버튼 */}
-					<div className="w-[190px] h-[60px] bg-light_yellow_nav rounded-full flex justify-center items-center shadow-lg">
-						<Link href={`parents/${childId}/shop/route`}>
+					<div className="w-[190px] h-[60px] bg-white rounded-full flex justify-center items-center shadow-lg">
+						<Link href={`/parents/${childId}/shop/${missionId}/route`}>
 							<button className="p-2">
 								<Image
 									src="/icons/map.svg"
@@ -39,7 +42,7 @@ const ParentsShoppingNavbar: React.FC<ParentsShoppingrNavbarProps> = ({
 
 						<div className="w-px h-8 bg-light_yellow_nav mx-2" />
 
-						<Link href={`parents/${childId}/shop/list`}>
+						<Link href={`/parents/${childId}/shop/${missionId}/list`}>
 							<button className="p-2">
 								<Image
 									src="/icons/basket.svg"
