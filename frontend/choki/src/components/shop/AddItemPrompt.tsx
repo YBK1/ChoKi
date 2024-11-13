d4d9890b98188a38d33db739804e0ade654a625a
인호현 <inhohyun36@gmail.com>
2024-11-13 14:12:11 +0900

    feat: 아이 장보기 새 물품 추가 이벤트 연동

--- frontend/choki/src/components/shop/AddItemPrompt.tsx
+++ frontend/choki/src/components/shop/AddItemPrompt.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
-const AddItemPrompt = () => (
+
+interface AddItemPromptProps {
+	onClick: () => void;
+}
+
+const AddItemPrompt: React.FC<AddItemPromptProps> = ({ onClick }) => (
 	<div className="flex items-center justify-center p-4 rounded-lg shadow-lg bg-light_yellow_kid mt-4 w-full max-w-md mx-auto">
 		<span className="text-lg font-semibold mr-2">물건을 추가로 담을래요!</span>
 		<Image
@@ -7,6 +12,8 @@ const AddItemPrompt = () => (
 			alt="Camera Icon"
 			width={24}
 			height={24}
+			onClick={onClick} // Image 클릭 시 onClick 이벤트 실행
+			className="cursor-pointer"
 		/>
 	</div>
 );
