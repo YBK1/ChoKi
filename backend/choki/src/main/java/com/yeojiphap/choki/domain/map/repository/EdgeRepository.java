c22edc8133b458dd527a932872c1bca7a3d88090
JiEung2 <wldmd5007@naver.com>
2024-11-14 17:00:08 +0900

    fix(edgeRepository): 겹치는 Edge를 가져오지 않기위해 Dinstinct 명령어 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/repository/EdgeRepository.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/repository/EdgeRepository.java
@@ -1,6 +1,7 @@
 package com.yeojiphap.choki.domain.map.repository;
 
 import com.yeojiphap.choki.domain.map.domain.Edge;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface EdgeRepository extends JpaRepository<Edge, Long> {
-    @Query("SELECT e FROM Edge e WHERE e.startNode.id IN :nodeIds OR e.endNode.id IN :nodeIds")
+    @EntityGraph(attributePaths = {"startNode", "endNode"})
+    @Query("SELECT DISTINCT e FROM Edge e WHERE e.startNode.id IN :nodeIds OR e.endNode.id IN :nodeIds")
     List<Edge> findEdgesBetweenNodes(@Param("nodeIds") List<Long> nodeIds);
 }
