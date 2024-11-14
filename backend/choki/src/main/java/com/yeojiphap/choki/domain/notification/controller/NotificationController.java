270e560c8b01d158ab055763539a16a215aecc80
김영빈 <kyb031700@naver.com>
2024-11-14 13:56:38 +0900

    feat: 알림 삭제 추가

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/notification/controller/NotificationController.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/notification/controller/NotificationController.java
@@ -3,10 +3,12 @@ package com.yeojiphap.choki.domain.notification.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yeojiphap.choki.domain.notification.dto.DeleteNotificationRequestDto;
 import com.yeojiphap.choki.domain.notification.service.NotificationService;
 import com.yeojiphap.choki.domain.shopping.dto.ShoppingCreateRequestDto;
 import com.yeojiphap.choki.global.ApiResponse;
@@ -23,5 +25,11 @@ public class NotificationController implements SpringDocNotificationController{
 	public ApiResponse listNotification(@PathVariable Long childId) {
 		// 일단 임시로 넣어놓자
 		return ApiResponse.success(HttpStatus.OK, notificationService.getNotifications(childId), "알림 조회 성공");
+	}
+
+	@PostMapping("/delete")
+	public ApiResponse deleteNotification(@RequestBody DeleteNotificationRequestDto deleteNotificationRequestDtongCreateRequestDto) {
+		notificationService.deleteNotification(deleteNotificationRequestDtongCreateRequestDto.getNotificationId());
+		return ApiResponse.success(HttpStatus.OK, "알림 삭제 성공");
 	}
 }
