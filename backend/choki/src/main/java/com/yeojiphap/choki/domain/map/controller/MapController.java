eaf3c187bbd2b40bf8943dbf75964e59f94d7b2f
JiEung2 <181757@jnu.ac.kr>
2024-11-15 21:25:14 +0900

    remove(mapController): 테스트용도로 사용하던 컨트롤러 삭제

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/controller/MapController.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/controller/MapController.java
@@ -1,6 +1,5 @@
 package com.yeojiphap.choki.domain.map.controller;
 
-import com.yeojiphap.choki.domain.map.domain.Location;
 import com.yeojiphap.choki.domain.map.dto.request.GuidedRouteRequest;
 import com.yeojiphap.choki.domain.map.dto.request.RouteRequest;
 import com.yeojiphap.choki.domain.map.service.MapService;
@@ -9,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 import static com.yeojiphap.choki.domain.map.message.MapSuccessMessage.GUIDED_ROUTES_SEARCH_SUCCESS;
 import static com.yeojiphap.choki.domain.map.message.MapSuccessMessage.GUIDED_ROUTE_SEARCH_SUCCESS;
@@ -23,7 +20,7 @@ public class MapController implements SpringDocMapController{
 
     @PostMapping("/route")
     public ApiResponse saveGuidedRoute(@RequestBody RouteRequest request){
-        return ApiResponse.success(HttpStatus.CREATED, mapService.saveGuidedRoute(request));
+        return ApiResponse.success(HttpStatus.CREATED, mapService.saveRoutes(request));
     }
 
     @GetMapping("/route/guide/list")
@@ -35,13 +32,5 @@ public class MapController implements SpringDocMapController{
     public ApiResponse getGuidedRoute(@PathVariable ObjectId guidedRouteId) {
         return ApiResponse.success(HttpStatus.OK, mapService.getGuidedRoute(new GuidedRouteRequest(guidedRouteId)),
                 GUIDED_ROUTE_SEARCH_SUCCESS.getMessage());
-    }
-
-    @GetMapping("/route/safe")
-    public List<Location> findPath(@RequestParam double startLatitude,
-                                   @RequestParam double startLongitude,
-                                   @RequestParam double goalLatitude,
-                                   @RequestParam double goalLongitude) {
-        return mapService.findOptimalPath(startLatitude, startLongitude, goalLatitude, goalLongitude);
     }
 }
