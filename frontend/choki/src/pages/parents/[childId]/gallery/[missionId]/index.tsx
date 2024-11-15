ec6c025b1eb526f19a80f779238cfe06ae2ae81c
김민주 <kminju2000@naver.com>
2024-11-15 15:26:52 +0900

    feat: 완료한 미션 상세 조회 구현

--- frontend/choki/src/pages/parents/[childId]/gallery/[missionId]/index.tsx
+++ frontend/choki/src/pages/parents/[childId]/gallery/[missionId]/index.tsx
@@ -1,1 +1,82 @@
-// 완료한 미션 상세 조회
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import MissionItem from '@/components/Common/MissionItem';
+import BottomNavbar from '@/components/Common/Navbar/BottomNavbar';
+import { getMissionDetail } from '@/lib/api/mission';
+import { useRouter } from 'next/router';
+
+export default function GalleryDetailPage() {
+	const router = useRouter();
+	const { missionId } = router.query;
+	const [missionDetail, setMissionDetail] =
+		useState<MissionDetailResponse | null>(null);
+
+	useEffect(() => {
+		const fetchMissionDetail = async () => {
+			if (missionId) {
+				try {
+					const response = await getMissionDetail(missionId as string);
+					console.log(response);
+					setMissionDetail(response);
+				} catch (error) {
+					console.error('Failed to fetch mission detail:', error);
+				}
+			}
+		};
+
+		fetchMissionDetail();
+	}, [missionId]);
+
+	if (!missionDetail) return;
+
+	return (
+		<>
+			<div className="flex flex-col w-full max-w-md mx-auto bg-light_yellow min-h-screen pb-10">
+				<div className="flex items-center p-4 border-b relative mt-3">
+					<button onClick={() => router.back()} className="p-2 absolute left-4">
+						<Image
+							src="/icons/previous_nav.svg"
+							alt="back"
+							width={24}
+							height={24}
+						/>
+					</button>
+					<h1 className="w-full text-center text-xl font-bold">
+						{missionDetail.completedAt?.split('T')[0].replace(/-/g, '.')}
+					</h1>
+				</div>
+
+				<div className="flex justify-center mt-5">
+					<MissionItem
+						type={missionDetail.type as MissionType}
+						content={missionDetail.content}
+					/>
+				</div>
+
+				<div className="flex justify-center items-center mt-2">
+					<div className="w-[330px] h-[330px] rounded-2xl bg-gray-100">
+						<div className="w-full h-full rounded-lg overflow-hidden flex items-center justify-center">
+							{missionDetail.image && (
+								<Image
+									src={missionDetail.image}
+									alt="Mission completed"
+									width={280}
+									height={280}
+									className="rounded-lg object-cover"
+								/>
+							)}
+						</div>
+					</div>
+				</div>
+
+				<div className="flex flex-col items-center">
+					<div className="text-lg font-bold mt-4 mb-2">한마디</div>
+					<div className="w-[310px] p-3 min-h-[20px] rounded-3xl border-2 border-black bg-gray-50">
+						{missionDetail.comment || '작성된 코멘트가 없습니다.'}
+					</div>
+				</div>
+			</div>
+			<BottomNavbar />
+		</>
+	);
+}
