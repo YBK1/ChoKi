701dee3b5f3318ec81e5f5241ea246bba264c78c
인호현 <inhohyun36@gmail.com>
2024-11-17 03:14:53 +0900

    fix: 아이가 추가한 물품 예외처리

--- frontend/choki/src/components/shop/ProductCard.tsx
+++ frontend/choki/src/components/shop/ProductCard.tsx
@@ -4,6 +4,14 @@ import Image from 'next/image';
 import { childWebSocketClient } from '@/lib/ws/WebSocketClient';
 import { missionIdAtom } from '@/atoms/shoppingAtom';
 import { useAtom } from 'jotai';
+
+interface CartItem {
+	barcode: string;
+	productName: string;
+	image: string;
+	quantity: number;
+}
+
 interface ShoppingCardProps {
 	role: string;
 	ParentsShoppingItem: {
@@ -25,6 +33,7 @@ const ProductCard: React.FC<ShoppingCardProps> = ({
 	onDelete,
 }) => {
 	const [missionId] = useAtom(missionIdAtom);
+
 	const handleDelete = () => {
 		if (ChildrenShoppingItem) {
 			const requestBody = {
@@ -36,34 +45,51 @@ const ProductCard: React.FC<ShoppingCardProps> = ({
 				'/pub/shopping/product/delete',
 				requestBody,
 			);
-
 			onDelete(ParentsShoppingItem.barcode);
 		}
 	};
+
+	const isParentItemValid =
+		ParentsShoppingItem.title &&
+		ParentsShoppingItem.count &&
+		ParentsShoppingItem.image &&
+		ParentsShoppingItem.barcode;
 
 	return (
 		<div className="relative flex items-stretch px-8 py-4 border border-gray-200 rounded-2xl max-w-xl mx-auto shadow-md">
 			<div className="absolute top-4 bottom-4 left-1/2 transform -translate-x-1/2 w-px bg-gray-200"></div>
 
+			{/* Left Section */}
 			<div className="flex flex-col items-center w-1/2 pr-6 h-full justify-center">
-				<div className="w-20 h-20 mb-0 flex items-center justify-center">
-					<Image
-						src={ParentsShoppingItem.image || '/icons/default_image.svg'}
-						alt="상품 이미지"
-						layout="fixed"
-						width={64}
-						height={96}
-					/>
-				</div>
-				<p className="text-gray-900 font-medium text-lg">
-					{ParentsShoppingItem.title}
-				</p>
-				<p className="text-gray-500 text-sm">
-					수량:{' '}
-					<span className="font-semibold">{ParentsShoppingItem.count}개</span>
-				</p>
+				{isParentItemValid ? (
+					<>
+						<div className="w-20 h-20 mb-0 flex items-center justify-center">
+							<Image
+								src={ParentsShoppingItem.image || '/icons/default_image.svg'}
+								alt="상품 이미지"
+								layout="fixed"
+								width={64}
+								height={96}
+							/>
+						</div>
+						<p className="text-gray-900 font-medium text-lg">
+							{ParentsShoppingItem.title}
+						</p>
+						<p className="text-gray-500 text-sm">
+							수량:{' '}
+							<span className="font-semibold">
+								{ParentsShoppingItem.count}개
+							</span>
+						</p>
+					</>
+				) : (
+					<p className="text-gray-500 text-center mt-10">
+						아이가 추가한 상품이에요
+					</p>
+				)}
 			</div>
 
+			{/* Right Section */}
 			<div className="flex flex-col items-center w-1/2 pl-6 h-full justify-center relative">
 				{role === 'CHILD' && ChildrenShoppingItem && (
 					<div className="absolute top-1 -right-4 cursor-pointer">
@@ -79,7 +105,7 @@ const ProductCard: React.FC<ShoppingCardProps> = ({
 				{role === 'PARENTS' && (
 					<div className="absolute top-1 -right-4 transform -translate-y-1/4 cursor-pointer">
 						<Image
-							src="/public/icons/mini_warning.svg"
+							src="/icons/mini_warning.svg"
 							alt="경고 아이콘"
 							width={30}
 							height={30}
