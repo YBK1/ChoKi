75fa4d2354b14f08ce454b6162b94355785da78a
JiEung2 <181757@jnu.ac.kr>
2024-11-17 01:49:59 +0900

    fix(routeRepository): 사용하지 않는 쿼리 삭제

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/repository/RouteRepository.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/repository/RouteRepository.java
@@ -4,8 +4,5 @@ import com.yeojiphap.choki.domain.map.domain.Route;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
-public interface RouteRepository extends MongoRepository<Route, ObjectId> {
-    List<Route> findByUserId(String userId);
+public interface RouteRepository extends MongoRepository<Route, ObjectId>, CustomRouteRepository {
 }
