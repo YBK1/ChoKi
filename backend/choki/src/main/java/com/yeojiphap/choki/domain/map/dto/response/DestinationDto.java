dfc8cb4e28fba91c64193e83809bfda28b23ac27
JiEung2 <181757@jnu.ac.kr>
2024-11-17 01:47:40 +0900

    fix(destinationDto): 목적지 dto에서 사용하지 않는 ObjectId 삭제

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/dto/response/DestinationDto.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/dto/response/DestinationDto.java
@@ -2,5 +2,5 @@ package com.yeojiphap.choki.domain.map.dto.response;
 
 import com.yeojiphap.choki.domain.map.domain.Location;
 
-public record DestinationDto(Location destination, String objectId) {
+public record DestinationDto(Location destination) {
 }
