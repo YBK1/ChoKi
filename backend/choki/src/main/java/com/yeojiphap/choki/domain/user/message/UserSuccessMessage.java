cae918f49bf3d6392cad65b800073a747f9db583
JiEung2 <181757@jnu.ac.kr>
2024-11-17 20:26:55 +0900

    fix(userSuccessMessage): 회원의 간단한 정보 전송 성공 메시지

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/user/message/UserSuccessMessage.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/user/message/UserSuccessMessage.java
@@ -10,7 +10,7 @@ public enum UserSuccessMessage {
     USER_ID_VALIDATION_SUCCESS("사용 가능한 아이디입니다."),
     NEARBY_USERS_FOUND("주변 사용자들 조회 성공"),
     USER_LEVEL_SEARCH_SUCCESS("회원 레벨 조회 성공"),
-    USER_ROLE_SEARCH_SUCCESS("회원 역할 조회 성공"),
+    USER_SIMPLE_INFO_SEARCH_SUCCESS("회원의 간단한 정보 조회 성공"),
     ;
 
     private final String message;
