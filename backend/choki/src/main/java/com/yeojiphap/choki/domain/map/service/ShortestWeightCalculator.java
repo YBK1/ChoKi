88ba9d35ff0f2381c2256ff76875cb723e409aa7
JiEung2 <wldmd5007@naver.com>
2024-11-18 09:35:10 +0900

    fix(shortestWeightCalculator): 가중치 수정

--- backend/choki/src/main/java/com/yeojiphap/choki/domain/map/service/ShortestWeightCalculator.java
+++ backend/choki/src/main/java/com/yeojiphap/choki/domain/map/service/ShortestWeightCalculator.java
@@ -3,6 +3,9 @@ package com.yeojiphap.choki.domain.map.service;
 public class ShortestWeightCalculator implements WeightCalculator {
     @Override
     public double calculate(double length, boolean hasCctv, boolean hasCrossing) {
-        return length;
+        double weight = length;
+        if (hasCctv) weight *= 1;
+        if (hasCrossing) weight *= 2.0;
+        return weight;
     }
 }
