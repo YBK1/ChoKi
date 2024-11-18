0a1d072cbe7843230fe7b5f6f8779461f9119573
MINJOO-KIM <gkminju2000@gmail.com>
2024-11-19 01:12:03 +0900

    fix: 이미지 크기 에러 해결

--- frontend/choki/src/components/Gallery/MissionList.tsx
+++ frontend/choki/src/components/Gallery/MissionList.tsx
@@ -23,17 +23,17 @@ const MissionList: React.FC<MissionListProps> = ({
 	};
 	return (
 		<div
-			className="relative w-[160px] h-[180px] bg-gray-100 rounded-3xl flex items-center justify-center cursor-pointer"
+			className="relative w-[160px] h-[185px] bg-gray-100 rounded-3xl flex flex-col items-center justify-center cursor-pointer"
 			onClick={handleMissionDetail}
 		>
-			<div className="absolute top-0 left-0 w-10 h-10">
+			<div className="absolute top-0 left-0 w-10 h-10 z-10">
 				<Image
 					src={MISSION_IMAGES[type as MissionType]}
 					alt={`${type} icon`}
 					fill
 				/>
 			</div>
-			<div className="flex flex-col items-center gap-1">
+			{/* <div className="flex flex-col items-center gap-1">
 				<Image
 					src={image}
 					alt="mission_img"
@@ -44,7 +44,19 @@ const MissionList: React.FC<MissionListProps> = ({
 				<span className="text-md font-semibold text-gray-600">
 					{completedAt}
 				</span>
+			</div> */}
+			<div className="relative w-[130px] h-[130px] rounded-2xl overflow-hidden mt-3">
+				<Image
+					src={image}
+					alt="mission_img"
+					fill
+					className="object-cover"
+					sizes="130px"
+				/>
 			</div>
+			<span className="text-sm mt-2 font-semibold text-gray-600">
+				{completedAt}
+			</span>
 		</div>
 	);
 };
